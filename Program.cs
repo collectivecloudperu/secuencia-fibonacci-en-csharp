88e5142242f67fa166379511014b2c195f9049af
Nube Colectiva <collectivecloudperu@users.noreply.github.com>
2025-08-18 22:21:19 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Ingresa un número (límite):");
+            int limite = int.Parse(Console.ReadLine());
+            Fibonacci fibonacciObject = new Fibonacci(limite);
+            Console.WriteLine(fibonacciObject.getLimitNumbers());
+        }
+    }
+
+    class Fibonacci
+    {
+        private int limite;
+
+        public Fibonacci(int limite)
+        {
+            this.limite = limite;
+        }
+
+        public string getLimitNumbers()
+        {
+            int a = 0, b = 1;
+            string resultado = "";
+
+            for (int i = 0; i < limite; i++)
+            {
+                resultado += a + " ";
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+
+            return resultado.Trim();
+        }
+    }
+}
